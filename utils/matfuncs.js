e62e04d78a62e0ed491c32dfecb51cd531e5fe6d
filbs111 <ed.filby@gmail.com>
2018-03-06 00:52:42 +0000

    move quaternions code to mathfuncs lib

--- utils/matfuncs.js
+++ utils/matfuncs.js
@@ -129,6 +129,121 @@ function rotate4matRows(mat, row1, row2, angle){
 	}
 }
 
+
+
+
+function scalarvectorprod(sca,vec){
+	return vec.map(function(val){return sca*val;});
+}
+
+function makemovequatpair(move){
+	//work out direction, length. move is a 3-vector
+	var lengthsq = move[0]*move[0] + move[1]*move[1] + move[2]*move[2];
+	var length = Math.sqrt(lengthsq);
+	var mult = Math.sin(length)/length;
+	var q = [Math.cos(length), mult*move[0], mult*move[1], mult*move[2]];
+	return [q,q];
+}
+
+function makerotatequatpair(rot){
+	//work out direction, length. move is a 3-vector
+	var lengthsq = rot[0]*rot[0] + rot[1]*rot[1] + rot[2]*rot[2];
+	var length = Math.sqrt(lengthsq);
+	var mult = Math.sin(length)/length;
+	var q = [Math.cos(length), -mult*rot[0], -mult*rot[1], -mult*rot[2]];
+	var qc = [Math.cos(length), mult*rot[0], mult*rot[1], mult*rot[2]];
+	return [q,qc];
+}
+
+function random_quat_pair(){
+	var qp=[];
+	qp.push(random_quaternion());
+	qp.push(random_quaternion());
+	return qp;
+}
+
+function random_quaternion(){
+	var q = [];
+	var sumsq=0;
+	for (var ii=0;ii<4;ii++){	//not an even distribution
+		q[ii] = Math.random()-0.5;
+	}
+    normalise_quat(q);
+	return q;
+}
+
+function normalise_qpair(qp){
+	normalise_quat(qp[0]);
+	normalise_quat(qp[1]);
+}
+
+function normalise_quat(q){
+	var sumsq=0;
+	for (var ii=0;ii<4;ii++){
+		sumsq+=q[ii]*q[ii];
+	}
+	var sum = Math.sqrt(sumsq);
+	for (var ii=0;ii<4;ii++){
+		q[ii]/=sum;
+	}
+}
+
+function convert_quats_to_4matrix(qpair){
+	var q1=qpair[0], q2=qpair[1];
+	
+	var m = mat4.create();
+	
+	var a1=q1[0];
+    var b1=q1[1];
+	var c1=q1[2];
+	var d1=q1[3];
+	var a2=q2[0];
+	var b2=q2[1];
+	var c2=q2[2];
+	var d2=q2[3];
+	
+	var a1a2=a1*a2, a1b2=a1*b2, a1c2=a1*c2, a1d2=a1*d2;
+	var b1a2=b1*a2, b1b2=b1*b2, b1c2=b1*c2, b1d2=b1*d2;
+	var c1a2=c1*a2, c1b2=c1*b2, c1c2=c1*c2, c1d2=c1*d2;
+	var d1a2=d1*a2, d1b2=d1*b2, d1c2=d1*c2, d1d2=d1*d2;
+
+	m[15]=a1a2-b1b2-c1c2-d1d2;	m[12]=-a1b2-b1a2+c1d2-d1c2;	m[13]=-a1c2-b1d2-c1a2+d1b2;	m[14]=-a1d2+b1c2-c1b2-d1a2;
+	m[3]=b1a2+a1b2-d1c2+c1d2;	m[0]=-b1b2+a1a2+d1d2+c1c2;	m[1]=-b1c2+a1d2-d1a2-c1b2;	m[2]=-b1d2-a1c2-d1b2+c1a2;
+	m[7]=c1a2+d1b2+a1c2-b1d2;	m[4]=-c1b2+d1a2-a1d2-b1c2;	m[5]=-c1c2+d1d2+a1a2+b1b2;	m[6]=-c1d2-d1c2+a1b2-b1a2;
+	m[11]=d1a2-c1b2+b1c2+a1d2;	m[8]=-d1b2-c1a2-b1d2+a1c2;	m[9]=-d1c2-c1d2+b1a2-a1b2;	m[10]=-d1d2+c1c2+b1b2+a1a2;
+	
+	//matrices(m,1,1)=asq+bsq-csq-dsq  :matrices(m,1,2)=2.0*(b*c-a*d)   :matrices(m,1,3)=2.0*(b*d+a*c)
+	//matrices(m,2,1)=2.0*(b*c+a*d)    :matrices(m,2,2)=asq-bsq+csq-dsq :matrices(m,2,3)=2.0*(c*d-a*b)
+	//matrices(m,3,1)=2.0*(b*d-a*c)    :matrices(m,3,2)=2.0*(c*d+a*b)   :matrices(m,3,3)=asq-bsq-csq+dsq
+	
+	return m;
+}
+
+function multiply_quaternions(b,a){	//note switched input order for consistency with matrix multiplication
+	var prod=[];
+	prod[0]=a[0]*b[0]-(a[1]*b[1]+a[2]*b[2]+a[3]*b[3]);       //s1s2 - v1.v2
+    //vector part of product= s1v2+s2v1+s3v3 +v1 x v2
+	prod[1]=a[0]*b[1]+b[0]*a[1]+ a[2]*b[3]-a[3]*b[2];
+	prod[2]=a[0]*b[2]+b[0]*a[2]+ a[3]*b[1]-a[1]*b[3];
+	prod[3]=a[0]*b[3]+b[0]*a[3]+ a[1]*b[2]-a[2]*b[1];
+	return prod;
+}
+
+function multiply_qpairs(qa,qb){
+	return [multiply_quaternions(qa[0],qb[0]), multiply_quaternions(qb[1],qa[1])];
+}
+
+
+function copy_quaternion(q_from,q_to){
+	for (var ii=0;ii<4;ii++){
+		q_to[ii] = q_from[ii];
+	}
+}
+
+
+
+
+
 function check4mat(mat){
 	//see whether becoming not normalised/orthogonal
 	//var resultsarrRow=[];
