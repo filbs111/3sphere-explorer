77915c03bb5dc0ac0ed05841a2457ae93da3d794
filbs111 <ed.filby@gmail.com>
2019-06-29 23:08:54 +0100

    request pointer lock 1 sec after fullscreen

--- utils/fullscreen.js
+++ utils/fullscreen.js
@@ -11,4 +11,8 @@ function goFullscreen(elem){
 	
 //	elem.requestPointerLock();	//going fullscreen appears to disable pointer lock, and adding this here doesn't help the 1st time through. race condition?
 								//hitting F again (while fullscreened) will cause pointer lock here to do something.
+	
+	setTimeout(function(){elem.requestPointerLock();},1000);	//works in testing on Firefox, my I5 machine. seems if delay too short, wont work.
+																//TODO try continually attempt pointer lock in fullscreen mode if
+																// document.pointerLockElement is null. maybe more reliable/faster
 }
