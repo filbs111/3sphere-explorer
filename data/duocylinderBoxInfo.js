9a7088279105c38b9ebf2e6dd55a459c7917f62d
filbs111 <ed.filby@gmail.com>
2019-07-20 19:28:25 +0100

    add more boxes so can test atmosphere shaders

--- data/duocylinderBoxInfo.js
+++ data/duocylinderBoxInfo.js
@@ -10,6 +10,16 @@ var duocylinderBoxInfo=(function generateBoxInfo(){
 	addBoxData(0,oneGridSquareOffset,hh, [0.4, 1.0, 0.4, 1.0]);				//green - along
 	addBoxData(0,0,oneGridSquareOffset*fudgeFact+hh, [0.4, 0.4, 1.0, 1.0]);	//blue - up
 
+	//an array of boxes, with view to testing atmosphere shader.
+	//this is a huge number of boxes. very inefficient. testing only. if want scene like this, combine into fewer objects (eg one)
+	for (var ii=0;ii<8;ii++){
+		for (var jj=0;jj<8;jj++){
+			for (var hi=-1;hi<10;hi++){
+				addBoxData(ii*0.4 +1,jj*0.4 -1,hi*0.05, [1.0, 1.0, 1.0, 1.0]);
+			}
+		}
+	}
+	
 	function addBoxData(aa, bb, hh, cc){
 		var boxMatrix = mat4.identity();
 		xyzrotate4mat(boxMatrix, [0,0,aa]);
