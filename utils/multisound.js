b94a8af80709af24194bd0fcbeabe9c7ea8ce020
filbs111 <ed.filby@gmail.com>
2020-04-19 01:33:04 +0100

    bin fallback for no web audio api
    waste of time to support both

--- utils/multisound.js
+++ utils/multisound.js
@@ -7,17 +7,12 @@ try {
 	audiocontext = new AudioContext();	//guess this is the bit that throws - can't do undefined()
 	usingAudioAPI = true;
 } catch(e) {
-	alert('Web Audio API is not supported in this browser.\nFalling back to audio elements.\nA modern web browser is recommended.');
+	alert('Web Audio API is not supported in this browser.\n sounds will not play');
 }
 
 	
 var MySound = (function(){
 	
-	//TODO some generalised sound function/constructor type thing, to allow new MySound(filename, instances);
-	//then can do this for each type of sound.
-	//don't know if that will be inefficient - whether play functions shared or not...
-
-	if (usingAudioAPI) {
 		audiocontext.resume();	//??
 		var globalGainNode = audiocontext.createGain();
 		globalGainNode.connect(audiocontext.destination);	//maybe inefficient - TODO bin globalGainNode?
@@ -74,38 +69,7 @@ var MySound = (function(){
 		mySound.prototype.setVolume = function(volume){
 			this.gainNode.gain.value = volume;
 		}		
-	} else {
-		//alert('Web Audio API is not supported in this browser.\nFalling back to audio elements.\nA modern web browser is recommended.');
-		
-		//return a constructor instead, so can use this to make multiple sounds
-		var mySound = function(soundAddress){
-			this.soundAddress = soundAddress;
-			this.audios = [];
-			this.nextAudio = 0;
-			this.typeVol = 0;
-			this.NUM_AUDIOS = 25;	//for audio element fallback only.
-			for (var ii=0;ii<this.NUM_AUDIOS;ii++){
-				this.audios.push({elem:new Audio(this.soundAddress), indivVol:0});
-			}
-		}
-		mySound.prototype.play = function(delay, vol){	//note that delay is unused
-			vol = vol || 1;
-			console.log("will play sound, using fallback audio tags, from: " + this.soundAddress);
-			var audio = this.audios[this.nextAudio];
-			audio.indivVol = vol;
-			audio.elem.volume = this.typeVol*vol;
-			audio.elem.play();
-			this.nextAudio = (this.nextAudio+1)%this.NUM_AUDIOS;
-		};
-		mySound.prototype.setVolume = function(volume){
-			this.typeVol = volume;
-			var audio;
-			for (var ii=0;ii<this.NUM_AUDIOS;ii++){
-				audio = this.audios[ii];
-				audio.elem.volume = volume*audio.indivVol;
-			}
-		}
-	}
+	
 	return mySound;
 })();
 
