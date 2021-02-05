filbs111/3sphere-explorer

commit 931f053905aa334f8eb618eddba2a55bd016b218

ui control of sea state

data/seaheight.js
@@ -10,14 +10,22 @@ var seaHeight = (function(){
 	
 	//precalculate as much as pos.
 	var waveList = [
-		{peakiness:0.15, waveCycles:[1.0,9.0]},
-		{peakiness:0.15, waveCycles:[-13.0,2.0]},
-		{peakiness:0.15, waveCycles:[3.0,-7.0]},
-		{peakiness:0.15, waveCycles:[-4.0,5.0]},
+		{waveCycles:[1.0,9.0]},
+		{waveCycles:[-13.0,2.0]},
+		{waveCycles:[3.0,-7.0]},
+		{waveCycles:[-4.0,5.0]},
 	];
 	
+	setPeakiness(0.15);	//TODO just call from outside before render
+
 	for (var wave of waveList){
 		addConstantsToWave(wave);
+	}
+
+	function setPeakiness(peakiness){
+		for (var wave of waveList){
+			wave.peakiness = peakiness;
+		}
 	}
 
 	function addConstantsToWave(wave){
@@ -56,7 +64,7 @@ var seaHeight = (function(){
 		get:function(pos,tt){	//pos is [xx,yy]
 			//wavePosAccum=new Array(3);	//might want to do this if don't want returned array to be affected by subsequent calls
 			wavePosAccum[0]=pos[0];
-			wavePosAccum[1]=pos[1]
+			wavePosAccum[1]=pos[1];
 			wavePosAccum[2]=zeroLevel;
 			currentTime=tt;
 			wavePosIn=pos;
@@ -67,6 +75,7 @@ var seaHeight = (function(){
 		
 			return wavePosAccum;
 		},
+		setPeakiness,
 		setZeroLevel:function(lev){zeroLevel=lev;}	//TODO instance sea objects with own zero level, to have different level for multiple seas without calling setZeroLevel repeatedly
 	}
 })();

js/main.js
@@ -1843,7 +1843,8 @@ function drawWorldScene(frameTime, isCubemapView) {
 	lastSeaTime=seaTime;	//for use in mechanics. TODO switch to using mechanics time for rendering instead
 	if (worldInfo.seaActive){
 		seaHeight.setZeroLevel(guiParams.seaLevel);
-		
+		seaHeight.setPeakiness(guiParams.seaPeakiness);	//TODO only call on ui change
+
 		//var seaHeight = getSeaHeight([0,0], [0.00005*(frameTime % 20000 )]);	//actually this is a position not a height . todo time conversion in one place 
 		var currentSeaHeight = getSeaHeight([0,0], seaTime);	//actually this is a position not a height . todo time conversion in one place 
 		var tau = Math.PI*2;
@@ -2619,7 +2620,7 @@ function drawWorldScene2(frameTime, wSettings, depthMap){	//TODO drawing using r
 	if (worldInfo.duocylinderModel!='none' && guiParams.display.zPrepass){
 		gl.depthFunc(gl.ALWAYS);	//TODO try no z check - since discarding with using depth texture, this check is redundant
 		gl.depthMask(false);
-		drawDuocylinderObject(wSettings, duocylinderObjects[worldInfo.duocylinderModel], 0,0, depthMap);
+		drawDuocylinderObject(wSettings, duocylinderObjects[worldInfo.duocylinderModel], 0,0,0, depthMap);
 		gl.depthFunc(gl.LESS);
 		gl.depthMask(true);
 	}
@@ -2629,7 +2630,7 @@ function drawWorldScene2(frameTime, wSettings, depthMap){	//TODO drawing using r
 
 	var seaTime = 0.00005*(frameTime % 20000 ); //20s loop	//note this is duplicated from drawWorldScene
 	if (worldInfo.seaActive){
-		drawDuocylinderObject(wSettings, duocylinderObjects['sea'], guiParams.seaLevel, seaTime, depthMap);
+		drawDuocylinderObject(wSettings, duocylinderObjects['sea'], guiParams.seaLevel, guiParams.seaPeakiness, seaTime, depthMap);
 	}
 
 
@@ -3288,6 +3289,7 @@ var guiParams={
 	world1:{duocylinderModel:"none",seaActive:false},
 	duocylinderRotateSpeed:0,
 	seaLevel:-0.012,
+	seaPeakiness:0.0,
 	drawShapes:{
 		boxes:{
 		'y=z=0':false,	//x*x+w*w=1
@@ -5471,7 +5473,7 @@ function performCommon4vecShaderSetup(activeShaderProgram, wSettings, logtag){	/
 	gl.uniform4fv(activeShaderProgram.uniforms.uDropLightPos, dropLightPos);
 	performGeneralShaderSetup(activeShaderProgram);
 }
-function drawDuocylinderObject(wSettings, duocylinderObj, zeroLevel, seaTime, depthMap){	
+function drawDuocylinderObject(wSettings, duocylinderObj, zeroLevel, seaPeakiness, seaTime, depthMap){	
 	var activeShaderProgram, selectedShaderSet;
 
 	//draw using z prepass if enabled. objects with substancial overdraw may draw faster, though increases num vertices drawn
@@ -5519,6 +5521,7 @@ function drawDuocylinderObject(wSettings, duocylinderObj, zeroLevel, seaTime, de
 		gl.useProgram(activeShaderProgram);
 		gl.uniform1f(activeShaderProgram.uniforms.uTime, seaTime);			
 		gl.uniform1f(activeShaderProgram.uniforms.uZeroLevel, zeroLevel);
+		gl.uniform1f(activeShaderProgram.uniforms.uPeakiness, seaPeakiness);
 	}
 		
 	if (activeShaderProgram.uniforms.uCameraWorldPos){	//extra info used for atmosphere shader

shaders/texmap-perpixel-vs-duocylinder-sea.glsl
@@ -14,6 +14,7 @@
 	uniform mat4 uPMatrix;
 	uniform float uTime;
 	uniform float uZeroLevel;
+	uniform float uPeakiness;
 	uniform vec4 uCameraWorldPos;
 	uniform vec4 uDropLightPos;	//position in camera frame ( 0,0,0,1 if light at camera )
 	varying vec4 transformedCoord;
@@ -93,10 +94,10 @@
 		vec3 vDerivativeWRTY = vec3(0.,1./CONST_TAU,0.);
 		
 		vec3 vModifiedVertexPosition = vec3( aVertexPosition.xy, uZeroLevel);
-		addWaveContribution(aVertexPosition, vModifiedVertexPosition, vDerivativeWRTX, vDerivativeWRTY, 0.15, vec2(1.0,9.0));
-		addWaveContribution(aVertexPosition, vModifiedVertexPosition, vDerivativeWRTX, vDerivativeWRTY, 0.15, vec2(-13.0,2.0));
-		addWaveContribution(aVertexPosition, vModifiedVertexPosition, vDerivativeWRTX, vDerivativeWRTY, 0.15, vec2(3.0,-7.0));
-		addWaveContribution(aVertexPosition, vModifiedVertexPosition, vDerivativeWRTX, vDerivativeWRTY, 0.15, vec2(-4.0,5.0));
+		addWaveContribution(aVertexPosition, vModifiedVertexPosition, vDerivativeWRTX, vDerivativeWRTY, uPeakiness, vec2(1.0,9.0));
+		addWaveContribution(aVertexPosition, vModifiedVertexPosition, vDerivativeWRTX, vDerivativeWRTY, uPeakiness, vec2(-13.0,2.0));
+		addWaveContribution(aVertexPosition, vModifiedVertexPosition, vDerivativeWRTX, vDerivativeWRTY, uPeakiness, vec2(3.0,-7.0));
+		addWaveContribution(aVertexPosition, vModifiedVertexPosition, vDerivativeWRTX, vDerivativeWRTY, uPeakiness, vec2(-4.0,5.0));
 		
 		vec3 surfNorm = normalize(cross(vDerivativeWRTX,vDerivativeWRTY));	//get surface normal by cross product of derivative vectors
 				

commit bd0afdac2ab4a1aec8964e1259e5f9787d5792fb

fix sea collision bug introduced by 931f053905aa334f8eb618eddba2a55bd016b218

data/seaheight.js
@@ -18,13 +18,10 @@ var seaHeight = (function(){
 	
 	setPeakiness(0.15);	//TODO just call from outside before render
 
-	for (var wave of waveList){
-		addConstantsToWave(wave);
-	}
-
 	function setPeakiness(peakiness){
 		for (var wave of waveList){
 			wave.peakiness = peakiness;
+			addConstantsToWave(wave);
 		}
 	}
 