d13a9b91a1e78f037f62b083bec2049068dc8b6b
Firstname Lastname <filbs111@protonmail.com>
2025-04-05 19:36:46 +0100

    bvh for triangle collision

--- utils/bvh_utils.js
+++ utils/bvh_utils.js
@@ -16,6 +16,13 @@ function createBvhFrom3dObjectData(sourceData){
     var tris = arrayToGroups(sourceData.indices, 3);
     var verts = arrayToGroups(sourceData.vertices, 3);
 
+    //calculate the whole object's bounds so fit within cube for morton/hilbert
+    //AABB would be tighter, but easy to just find furthest point for origin for bounding sphere, wrap this sphere in a cube.
+    var boundingSphereDiam = 2* Math.sqrt(verts.reduce((greatest, current) => { 
+        return Math.max(greatest, current.reduce((cumul, val) => {return cumul + val*val}, 0));
+    }, 0));
+    console.log("boundingSphereDiam:" + boundingSphereDiam);
+
     var temp3Vec = [...new Array(3)];
 
     //for each triangle, create an AABB.
@@ -28,6 +35,8 @@ function createBvhFrom3dObjectData(sourceData){
         var minAABB = components.map( minAABBPointsForComponent => Math.min.apply(null, minAABBPointsForComponent));
         var maxAABB = components.map( maxAABBPointsForComponent => Math.max.apply(null, maxAABBPointsForComponent));
 
+        var normalisedBoxCentre = minAABB.map( (minval, ii) => {return (minval + maxAABB[ii])/boundingSphereDiam;} );  //between -1, 1
+        var centreMorton = morton3(normalisedBoxCentre);
         //TODO calculate centre position so can morton/hilbert order for grouping into tree...
 
         //calculate triangle normal.
@@ -56,13 +65,54 @@ function createBvhFrom3dObjectData(sourceData){
             normal,
             distFromOrigin,
             edgeData,
-            AABB: [minAABB, maxAABB] 
+            AABB: [minAABB, maxAABB],
+            centreMorton    //note only used for ordering. TODO remove once used?
         }
     });
 
+    trisWithAABB.sort((a,b) => a.centreMorton - b.centreMorton);  //sort by morton code.
+
     return {
         verts,   //suspect only require vertex data.
-        tris: trisWithAABB
+        tris: generateBvh(trisWithAABB, 4)
+    }
+
+    function generateBvh(items, groupSize){
+        console.log({items});
+
+        if (items.length < 2){  //TODO why is length ever 0?
+            //console.log("returning because items of length: " + items.length);
+            //console.log(items);
+            return items[0];
+        }
+    
+        var groups = arrayToGroups(items, groupSize);
+        console.log("NUM GROUPS:" + groups.length);
+        console.log({groups});
+
+        var nextLayerUp = groups.map(group => {
+            
+            var minAABBPoints = temp3Vec.map( (_,component) => group.map(item => item.AABB[0][component]));
+            var minAABB = minAABBPoints.map( minAABBPointsForComponent => Math.min.apply(null, minAABBPointsForComponent));
+    
+            var maxAABBPoints = temp3Vec.map( (_,component) => group.map(item => item.AABB[1][component]));
+            var maxAABB = maxAABBPoints.map( maxAABBPointsForComponent => Math.max.apply(null, maxAABBPointsForComponent));
+    
+            // var morton = [
+            //     Math.min.apply(null, group.map(item => item.morton[0])),
+            //     Math.max.apply(null, group.map(item => item.morton[1]))
+            // ];
+            // morton for AABB corners. maybe useful for fast coarse AABB check (only compare single value, but false positives)
+    
+            var toReturn = {
+                group,
+                AABB: [minAABB, maxAABB],
+                // morton
+            };
+            return toReturn;
+        });
+    
+        return generateBvh(nextLayerUp, groupSize);
     }
 }
 
@@ -71,9 +121,11 @@ function bvhRayOverlapTest(rayStart, rayEnd, bvh){
     var rayAABB = [Math.min, Math.max].map(minmaxfunc => {
         return tempVec3.map( (unused,ii) => {return minmaxfunc(rayStart[ii], rayEnd[ii]);});
     });
-    console.log(rayAABB);
-    for (var ii=0;ii< bvh.tris.length; ii++){
-        var thisTri = bvh.tris[ii];
+    
+    var possibles = collisionTestBvh(rayAABB, bvh.tris);
+    
+    for (var ii=0;ii< possibles.length; ii++){
+        var thisTri = possibles[ii];
         if (aabbsOverlap(rayAABB, thisTri.AABB)){
             startDistFromPlane = dotProduct(thisTri.normal, rayStart) - thisTri.distFromOrigin;
             endDistFromPlane = dotProduct(thisTri.normal, rayEnd) - thisTri.distFromOrigin;
@@ -86,37 +138,56 @@ function bvhRayOverlapTest(rayStart, rayEnd, bvh){
     return false;
 }
 
-function bvhSphereOverlapTest(spherePos, sphereRad, bvh){
+//this returns possible colliding bvh nodes in the group.
+function collisionTestBvh(aabb, bvh){
+    
+    if (!bvh.group){ //is a leaf node
+        //console.log("returning because bvh has no group");
+        //console.log(bvh);
 
-    var testAABB = [
-        spherePos.map(cc => cc-sphereRad),
-        spherePos.map(cc => cc+sphereRad)
-    ];
-
-    //TODO generalise this to take 2 bvhs or items with aabbs? 
-    //currently bvh is just an array of triangles with AABBS
-    for (var ii=0;ii< bvh.tris.length; ii++){
-        if (aabbsOverlap(testAABB, bvh.tris[ii].AABB)){
-            //check for distance from triangle plane. 
-            //with just this, result is passable so far (teapot object, shots)
-            if (Math.abs(pointSignedDistanceFromPlane(spherePos, bvh.tris[ii])) > sphereRad){
-                return false;
-            }
-            //check that within edge planes too.
-            //with this is like point collision with triangular prism minkowski sum
-            for (var ee=0;ee<3;ee++){
-                if (pointSignedDistanceFromPlane(spherePos, bvh.tris[ii].edgeData[ee]) > sphereRad){
-                    return false;
-                }
-            }
-            return true;
-            //TODO correct distance from triangle for sphere - minkowski sum should have rounded edges and corners
-            // can do with Separating Axis Test for edges, points
-            // without this might suspect may observe player object bumping on edges,points when sliding on object surface.
-        }
+        return bvh;
     }
-    return false;
+
+    var filteredGroup =  bvh.group.filter(
+            item =>
+            aabbsOverlap(aabb, item.AABB)
+        );
+    
+    return filteredGroup.map(group2 => collisionTestBvh(aabb, group2)).flat();
 }
+
+//currently unused. TODO use for player sphere collision with level?
+// function bvhSphereOverlapTest(spherePos, sphereRad, bvh){
+
+//     var testAABB = [
+//         spherePos.map(cc => cc-sphereRad),
+//         spherePos.map(cc => cc+sphereRad)
+//     ];
+
+//     //TODO generalise this to take 2 bvhs or items with aabbs? 
+//     //currently bvh is just an array of triangles with AABBS
+//     for (var ii=0;ii< bvh.tris.length; ii++){
+//         if (aabbsOverlap(testAABB, bvh.tris[ii].AABB)){
+//             //check for distance from triangle plane. 
+//             //with just this, result is passable so far (teapot object, shots)
+//             if (Math.abs(pointSignedDistanceFromPlane(spherePos, bvh.tris[ii])) > sphereRad){
+//                 return false;
+//             }
+//             //check that within edge planes too.
+//             //with this is like point collision with triangular prism minkowski sum
+//             for (var ee=0;ee<3;ee++){
+//                 if (pointSignedDistanceFromPlane(spherePos, bvh.tris[ii].edgeData[ee]) > sphereRad){
+//                     return false;
+//                 }
+//             }
+//             return true;
+//             //TODO correct distance from triangle for sphere - minkowski sum should have rounded edges and corners
+//             // can do with Separating Axis Test for edges, points
+//             // without this might suspect may observe player object bumping on edges,points when sliding on object surface.
+//         }
+//     }
+//     return false;
+// }
 
 function aabbsOverlap(aabb1, aabb2){
     var intersection = true;
@@ -165,4 +236,56 @@ function arrayToGroups(initialArray, groupSize){
           result.push(array.slice(index, index + groupSize));
         return result;
       }, []);
+}
+
+function morton3(threevec){
+    //might be slow and crap! 
+    //TODO round up or down the response to this?
+    var bitarrays = threevec.map(xx => {
+
+        var intnum = (xx+1)*512;
+        intnum = Math.min(intnum, 1023);  //because could be 1024 before this? 
+        intnum = Math.max(intnum, 0);
+
+        var bits = [...Array(10)].map((x,i)=>intnum>>i&1);
+            //least to most significant 10 bits
+
+        return bits
+    } ); //map 0 to 2 to 0 to 2^32
+
+    //console.log(bitarrays);
+
+    var morton 
+        = (bitarrays[0][9] << 29)
+        + (bitarrays[1][9] << 28)
+        + (bitarrays[2][9] << 27)
+        + (bitarrays[0][8] << 26)
+        + (bitarrays[1][8] << 25)
+        + (bitarrays[2][8] << 24)
+        + (bitarrays[0][7] << 23)
+        + (bitarrays[1][7] << 22)
+        + (bitarrays[2][7] << 21)
+        + (bitarrays[0][6] << 20)
+        + (bitarrays[1][6] << 19)
+        + (bitarrays[2][6] << 18)
+        + (bitarrays[0][5] << 17)
+        + (bitarrays[1][5] << 16)
+        + (bitarrays[2][5] << 15)
+        + (bitarrays[0][4] << 14)
+        + (bitarrays[1][4] << 13)
+        + (bitarrays[2][4] << 12)
+        + (bitarrays[0][3] << 11)
+        + (bitarrays[1][3] << 10)
+        + (bitarrays[2][3] << 9)
+        + (bitarrays[0][2] << 8)
+        + (bitarrays[1][2] << 7)
+        + (bitarrays[2][2] << 6)
+        + (bitarrays[0][1] << 5)
+        + (bitarrays[1][1] << 4)
+        + (bitarrays[2][1] << 3)
+        + (bitarrays[0][0] << 2)
+        + (bitarrays[1][0] << 1)
+        + (bitarrays[2][0]);
+
+    return morton;
 }
