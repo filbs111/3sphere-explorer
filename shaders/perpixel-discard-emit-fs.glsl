filbs111/3sphere-explorer

commit 3be9b4583080b8979a3a354a83731bae116c1bc0

fix z-depth interpolation problem seen on objs with long edges

shaders/perpixel-discard-emit-fs.glsl
@@ -87,7 +87,13 @@ out vec4 fragColor;
 		
 		fragColor.a = uColor.a * vVertexColor.r;	//TODO confirm check logic for transparent objects
 #ifdef CUSTOM_DEPTH
-		float depthVal = .5*(vZW.x/vZW.y) + .5;
+
+		// here x=w, y=z, but also confusingly switched by pMatrix ! 
+		//TODO if this works, don't bother creating vZW in vert shader.
+		if (vZW.y > -1.){discard;} //other side of world. shouldn't happen much with culling. TODO discard earlier?
+		float depthVal = .3183*atan((vZW.x*2.)/(vZW.y+1.)) + .5;
+		
+		//float depthVal = .5*(vZW.x/vZW.y) + .5;
 		gl_FragDepth = depthVal;
 		fragColor.a = depthVal;
 #endif

shaders/cubemap-fs.glsl
@@ -107,9 +107,10 @@ out vec4 fragColor;
 #endif
 
 #ifdef CUSTOM_DEPTH
-		gl_FragDepth = .5*(vZW.x/vZW.y) + .5;
-		//vec2 normZW= normalize(vZW);
-		//gl_FragDepth = .5*(normZW.x/normZW.y) + .5;
+		// here x=w, y=z, but also confusingly switched by pMatrix ! 
+		//TODO if this works, don't bother creating vZW in vert shader.
+		if (vZW.y > -1.){discard;} //other side of world. shouldn't happen much with culling. TODO discard earlier?
+		gl_FragDepth = .3183*atan((vZW.x*2.)/(vZW.y+1.)) + .5;
 #endif
 }
 

shaders/perpixel-discard-fs.glsl
@@ -109,7 +109,10 @@ out vec4 fragColor;
 		
 		fragColor.a =uColor.a;	//TODO confirm check logic for transparent objects
 #ifdef CUSTOM_DEPTH
-		float depthVal = .5*(vZW.x/vZW.y) + .5;
+		// here x=w, y=z !	//TODO if this works, don't bother creating vZW in vert shader.
+		if (vZW.y > -1.){discard;}
+		float depthVal = .3183*atan((vZW.x*2.)/(vZW.y+1.)) + .5;
+
 		gl_FragDepth = depthVal;
 		fragColor.a = depthVal;
 #endif

shaders/perpixel-transparent-discard-fs.glsl
@@ -28,6 +28,9 @@ out vec4 fragColor;
 		//fragColor = vec4(uEmitColor, fog*light);
 		fragColor = vec4(uEmitColor, fog);
 #ifdef CUSTOM_DEPTH
-		gl_FragDepth = .5*(vZW.x/vZW.y) + .5;
+		// here x=w, y=z, but also confusingly switched by pMatrix ! 
+		//TODO if this works, don't bother creating vZW in vert shader.
+		if (vZW.y > -1.){discard;} //other side of world. shouldn't happen much with culling. TODO discard earlier?
+		gl_FragDepth = .3183*atan((vZW.x*2.)/(vZW.y+1.)) + .5;
 #endif
 	}

shaders/simple-nofog-fs.glsl
@@ -9,6 +9,9 @@ out vec4 fragColor;
 void main(void) {
 	fragColor = vec4(1.0,0.0,1.0,1.0);	//magenta
 #ifdef CUSTOM_DEPTH
-	gl_FragDepth = .5*(vZW.x/vZW.y) + .5;
+	// here x=w, y=z, but also confusingly switched by pMatrix ! 
+	//TODO if this works, don't bother creating vZW in vert shader.
+	if (vZW.y > -1.){discard;} //other side of world. shouldn't happen much with culling. TODO discard earlier?
+	gl_FragDepth = .3183*atan((vZW.x*2.)/(vZW.y+1.)) + .5;
 #endif
 }

shaders/texmap-fs.glsl
@@ -20,8 +20,13 @@ out vec4 fragColor;
 	void main(void) {
 		//gl_FragColor = uColor*texture(uSampler, vTextureCoord);
 
+		// here x=w, y=z, but also confusingly switched by pMatrix ! 
+		//TODO if this works, don't bother creating vZW in vert shader.
+		if (vZW.y > -1.){discard;} //other side of world. shouldn't happen much with culling. TODO discard earlier?
+		float depthVal = .3183*atan((vZW.x*2.)/(vZW.y+1.)) + .5;	
+
 #ifdef CUSTOM_DEPTH
-		gl_FragDepth = .5*(vZW.x/vZW.y) + .5;
+		gl_FragDepth = depthVal;
 #endif
 
 #ifdef MAPPROJECT_ACTIVE
@@ -41,6 +46,5 @@ out vec4 fragColor;
 		//fragColor = uColor*fog*textureProj(uSampler, vTextureCoord) + (1.0-fog)*uFogColor;
 		//fragColor = (1.0-fog)*uFogColor;
 
-		float depthVal = .5*(vZW.x/vZW.y) + .5;	//assumes passing though vZW.
 		fragColor.a = depthVal;
 	}

shaders/texmap-perpixel-discard-fs.glsl
@@ -43,7 +43,7 @@ out vec4 fragColor;
 	void main(void) {
 #ifdef DEPTH_AWARE
 		float currentDepth =  textureProj(uSamplerDepthmap, vec3(.5,.5,1.)*vScreenSpaceCoord.xyz + vec3(.5,.5,0.)*vScreenSpaceCoord.z).r;
-		float newDepth = .5*(vZW.x/vZW.y) + .5;	//this is duplicate of custom depth calculation
+		float newDepth = .3183*atan((vZW.x*2.)/(vZW.y+1.)) + .5;	//this is duplicate of custom depth calculation
 		if (newDepth>currentDepth){
 			discard;
 		}
@@ -53,7 +53,10 @@ out vec4 fragColor;
 	//, but still want to write depth when drawing sea, which this shader used for
 	//TODO separate shader for sea if impacts perf
 #ifdef CUSTOM_DEPTH
-		float depthVal = .5*(vZW.x/vZW.y) + .5;
+		// here x=w, y=z, but also confusingly switched by pMatrix ! 
+		//TODO if this works, don't bother creating vZW in vert shader.
+		if (vZW.y > -1.){discard;} //other side of world. shouldn't happen much with culling. TODO discard earlier?
+		float depthVal = .3183*atan((vZW.x*2.)/(vZW.y+1.)) + .5;
 		gl_FragDepth = depthVal;
 #endif
 //#endif

shaders/texmap-perpixel-discard-normalmap-efficient-fs.glsl
@@ -57,7 +57,7 @@ out vec4 fragColor;
 
 #ifdef DEPTH_AWARE
 		float currentDepth =  textureProj(uSamplerDepthmap, vec3(.5,.5,1.)*vScreenSpaceCoord.xyz + vec3(.5,.5,0.)*vScreenSpaceCoord.z).r;
-		float newDepth = .5*(vZW.x/vZW.y) + .5;	//this is duplicate of custom depth calculation
+		float newDepth = .3183*atan((vZW.x*2.)/(vZW.y+1.)) + .5;	//this is duplicate of custom depth calculation
 		if (newDepth>currentDepth){
 			discard;
 		}
@@ -65,7 +65,10 @@ out vec4 fragColor;
 
 #ifndef DEPTH_AWARE
 #ifdef CUSTOM_DEPTH
-		gl_FragDepth = .5*(vZW.x/vZW.y) + .5;
+		// here x=w, y=z, but also confusingly switched by pMatrix ! 
+		//TODO if this works, don't bother creating vZW in vert shader.
+		if (vZW.y > -1.){discard;} //other side of world. shouldn't happen much with culling. TODO discard earlier?
+		gl_FragDepth = .3183*atan((vZW.x*2.)/(vZW.y+1.)) + .5;
 #endif
 #endif
 

commit c279fa18aa1a1f37ee2226826daced5f3b396c53

fix drawing of thrusters, seems broken since 3be9b45

shaders/perpixel-discard-emit-fs.glsl
@@ -97,7 +97,6 @@ out vec4 fragColor;
 
 		//float depthVal = .5*(vZW.x/vZW.y) + .5;
 		gl_FragDepth = depthVal;
-		fragColor.a = depthVal;
 #endif
 	}
 