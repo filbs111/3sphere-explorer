1b02da57581fb4508e2c3dbdb32f186b3a74a32c
Firstname Lastname <filbs111@protonmail.com>
2025-10-19 21:01:48 +0100

    deduplicate 4d tri data cache creation code

--- data/tennisBallLoader.js
+++ data/tennisBallLoader.js
@@ -221,47 +221,7 @@ function loadGridData(toLoad, generateCollisionData){
 
 		toLoad.collisionTriangleData = generateBvh(allTris, temp4vec, 8);
 
-		toLoad.collisionTriangleData.cache = ((facesAsTriVerts, verts4d) => {
-
-			var maxCacheEntries = 1024;	//TODO tune.
-			var cache = new Map();	//TODO LRU cache (limited size). for now, just store everything.
-			var cacheQueries = 0;
-			var cacheMisses = 0;
-
-			function getTriDataForFace(faceIdx){
-				cacheQueries++;
-				var cachedVal = cache.get(faceIdx);
-				if (cachedVal){
-					//move to front of cache (most recently accessed)
-
-					cache.delete(faceIdx);
-					cache.set(faceIdx, cachedVal);
-					
-					return cachedVal;
-				}
-				cacheMisses++;
-
-				if (cache.size >= maxCacheEntries){	//equals check should be sufficient.
-					cache.delete(cache.keys().next().value);	//delete least recently accessed cache entry
-				}
-				
-				var calculatedVal = makeCollisionDataForTriangle4dNoAABB(facesAsTriVerts[faceIdx].map(vv => verts4d[vv]));
-				
-				cache.set(faceIdx, calculatedVal);
-				return calculatedVal;
-			}
-
-			return {
-				getTriDataForFace,
-				getStats(){ return {
-					size: cache.size,
-					cacheQueries,
-					cacheMisses,
-					missRatio: cacheMisses/cacheQueries
-				}}
-			}
-		})(facesAsTriVerts, verts4d);	//don't really need to pass this in since accessible here, but will want if move function outside
-
+		toLoad.collisionTriangleData.cache = createTriCollisionDataCach(facesAsTriVerts, verts4d);
 	}
 
 };
@@ -403,4 +363,51 @@ function makeCollisionDataForTriangle4dNoAABB(triVerts){
 		edges,
 		edgeGcs
 	};
+}
+
+function createTriCollisionDataCach(facesAsTriVerts, verts4d){
+	var maxCacheEntries = 1024;	//TODO tune.
+	var cache = new Map();	//TODO LRU cache (limited size). for now, just store everything.
+	var cacheQueries = 0;
+	var cacheMisses = 0;
+
+	function getTriDataForFace(faceIdx){
+		cacheQueries++;
+		var cachedVal = cache.get(faceIdx);
+		if (cachedVal){
+			//move to front of cache (most recently accessed)
+
+			cache.delete(faceIdx);
+			cache.set(faceIdx, cachedVal);
+			
+			return cachedVal;
+		}
+		cacheMisses++;
+
+		if (cache.size >= maxCacheEntries){	//equals check should be sufficient.
+			cache.delete(cache.keys().next().value);	//delete least recently accessed cache entry
+		}
+		
+		console.log({
+			info:"attempting to makeCollisionDataForTriangle4dNoAABB",
+			faceIdx,
+			facesAsTriVerts,
+			verts4d
+		})
+
+		var calculatedVal = makeCollisionDataForTriangle4dNoAABB(facesAsTriVerts[faceIdx].map(vv => verts4d[vv]));
+		
+		cache.set(faceIdx, calculatedVal);
+		return calculatedVal;
+	}
+
+	return {
+		getTriDataForFace,
+		getStats(){ return {
+			size: cache.size,
+			cacheQueries,
+			cacheMisses,
+			missRatio: cacheMisses/cacheQueries
+		}}
+	}
 }
