afc1b8655d5b9e06e94bad8e130610a6d1f87f5b
Firstname Lastname <filbs111@protonmail.com>
2025-10-19 17:06:29 +0100

    least recently used (LRU) cache to tri collision data

--- data/tennisBallLoader.js
+++ data/tennisBallLoader.js
@@ -223,6 +223,7 @@ function loadGridData(toLoad, generateCollisionData){
 
 		toLoad.collisionTriangleData.cache = ((facesAsTriVerts, verts4d) => {
 
+			var maxCacheEntries = 1024;	//TODO tune.
 			var cache = new Map();	//TODO LRU cache (limited size). for now, just store everything.
 			var cacheQueries = 0;
 			var cacheMisses = 0;
@@ -230,9 +231,22 @@ function loadGridData(toLoad, generateCollisionData){
 			function getTriDataForFace(faceIdx){
 				cacheQueries++;
 				var cachedVal = cache.get(faceIdx);
-				if (cachedVal){return cachedVal;}
+				if (cachedVal){
+					//move to front of cache (most recently accessed)
+
+					cache.delete(faceIdx);
+					cache.set(faceIdx, cachedVal);
+					
+					return cachedVal;
+				}
 				cacheMisses++;
+
+				if (cache.size >= maxCacheEntries){	//equals check should be sufficient.
+					cache.delete(cache.keys().next().value);	//delete least recently accessed cache entry
+				}
+				
 				var calculatedVal = makeCollisionDataForTriangle4dNoAABB(facesAsTriVerts[faceIdx].map(vv => verts4d[vv]));
+				
 				cache.set(faceIdx, calculatedVal);
 				return calculatedVal;
 			}
