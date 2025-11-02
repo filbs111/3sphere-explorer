filbs111/3sphere-explorer

commit f29b0a0e0bd018fa7042bb5041141e83010f0eb6

spin non-terrain objects with world

js/main.js
@@ -2473,22 +2473,17 @@ function drawWorldScene(frameTime, isCubemapView, viewSettings, wSettings) {
 		
 	uniform4fvSetter.setIfDifferent(activeShaderProgram, "uColor", colorArrs.darkGray);
 
-	var cubeFrames = bvhObjsForWorld[worldA].objList.filter(objInfo=> objInfo.bvh == cubeFrameBvh)	//TODO prefilter
-	if (cubeFrames.length>0){
-		drawArrayOfModels2(cubeFrames, cubeFrameBuffers, activeShaderProgram);
-	}
-	var dodecaFrames = bvhObjsForWorld[worldA].objList.filter(objInfo=> objInfo.bvh == dodecaFrameBvh2)	//TODO prefilter
-	if (dodecaFrames.length>0){
-		drawArrayOfModels2(dodecaFrames, dodecaFrameBuffers2, activeShaderProgram);
-	}
-	var tetraFrames = bvhObjsForWorld[worldA].objList.filter(objInfo=> objInfo.bvh == tetraFrameBvh)	//TODO prefilter
-	if (tetraFrames.length>0){
-		drawArrayOfModels2(tetraFrames, tetraFrameBuffers, activeShaderProgram);
-	}
-	var octoFrames = bvhObjsForWorld[worldA].objList.filter(objInfo=> objInfo.bvh == octoFrameBvh)	//TODO prefilter
-	if (octoFrames.length>0){
-		drawArrayOfModels2(octoFrames, octoFrameSubdivBuffers, activeShaderProgram);
-	}
+	[
+		{bvh:cubeFrameBvh, buffers:cubeFrameBuffers},
+		{bvh:dodecaFrameBvh2, buffers:dodecaFrameBuffers2},
+		{bvh:tetraFrameBvh, buffers:tetraFrameBuffers},
+		{bvh:octoFrameBvh, buffers:octoFrameSubdivBuffers}
+	].forEach(objTypeInfo => {
+		var objs = bvhObjsForWorld[worldA].objList.filter(objInfo=> objInfo.bvh == objTypeInfo.bvh)	//TODO prefilter
+		if (objs>0){
+			drawArrayOfModels2(objs, objTypeInfo.buffers, activeShaderProgram);
+		}
+	});
 	
 	//todo this should take buffers, shaders and call prepBuffersForDrawing, drawObjectFromPreppedBuffers
 	function drawArrayOfModels(cellMats, cullRad, buffers, shaderProg){
@@ -2535,9 +2530,13 @@ function drawWorldScene(frameTime, isCubemapView, viewSettings, wSettings) {
 				var thisObj = objDataArr[dd];
 				var myscale = thisObj.scale;
 				gl.uniform3f(activeShaderProgram.uniforms.uModelScale, myscale,myscale,myscale);
+
 				mat4.set(invertedWorldCamera, mvMatrix);
+				rotate4mat(mvMatrix, 0, 1, duocylinderSpin);
 				mat4.multiply(mvMatrix,thisObj.mat);
-				mat4.set(thisObj.mat, mMatrix);	//not needed in all shaders
+
+				mat4.identity(mMatrix);rotate4mat(mMatrix, 0, 1, duocylinderSpin);
+				mat4.multiply(mMatrix, thisObj.mat);	//not needed in all shaders
 				drawFunc2();
 			}
 		}

js/player_mechanics.js
@@ -411,7 +411,7 @@ var playerMechanics = (() => {
             //TODO update variables to do with duocylinder between substeps? 
             terrainCollisionFunc(ii==0 ? terrainAudio : false);
 
-            processTriangleObjectCollisionFast();   //collision detection
+            processTriangleObjectCollisionFast(timestepFraction);   //collision detection
             processTriangleTerrainCollisionFast(timestepFraction);  //for terrain objects using 4d tris
 
             rotatePlayer(scalarvectorprod(subTimeStep * rotateSpeed,playerAngVelVec));
@@ -438,7 +438,7 @@ var playerMechanics = (() => {
         myAudioPlayer.setJetSound({delay:0, gain:thrustVolume, pan:0});
 
         
-        function processTriangleObjectCollisionFast(){
+        function processTriangleObjectCollisionFast(timestepFraction){
 
             if (guiParams.debug.drawPlayerPosMarkers){
                 debugDraw.addTestPoint(playerContainer.matrix, colorArrs.white);
@@ -446,14 +446,28 @@ var playerMechanics = (() => {
 
             var worldBvhObj = bvhObjsForWorld[playerContainer.world];
 
-            var initialCandidates = guiParams.debug.worldBvhCollisionTestPlayer ? getFastPossibles():worldBvhObj.objList;
+            var wSettings = guiSettingsForWorld[playerContainer.world];
+            var dcSpin = wSettings.spin * timestepFraction + (1-timestepFraction)*wSettings.spinOld;
+
+            //var initialCandidates = guiParams.debug.worldBvhCollisionTestPlayer ? getFastPossibles():worldBvhObj.objList;
+            var initialCandidates = worldBvhObj.objList;   //switch off initially for rotating objects (TODO reinstate by calculating player pos in rotated frame)
+
+
+            var rotatedRefMat = mat4.identity();
+            rotate4mat(rotatedRefMat, 0, 1, dcSpin);
 
 
             if (guiParams["player model"] == "convexHullTest"){
 
                 var hackObjInfoArr = initialCandidates.map(objInfo => {
-                    var mat = mat4.create(objInfo.mat);
-                    //rotate4mat(mat, 0, 1, dcSpin);    //TODO SPIN?
+                    //var mat = mat4.create(objInfo.mat);
+                    //rotate4mat(mat, 0, 1, dcSpin);  //this works for terrain objects, but not here! perhaps terrain object orientation just happens to 
+                        //work with this instruction...
+                    
+                    var mat = mat4.create(rotatedRefMat);
+                    mat4.multiply(mat, objInfo.mat);
+                    
+
                     var transposedMat = mat4.create(mat);
                     mat4.transpose(transposedMat);
                     return {

utils/bvh_utils.js
@@ -1665,10 +1665,11 @@ function minMaxDistanceFromPointToBoundingSphere(pointPos, spherePos, sphereRad)
 function rayBvhCollision(cameraRayStartPos, cameraRayEndPos, cameraRayStartPosSpun, cameraRayEndPosSpun, world){
 
     var collided = false;
-    var objectIsSpinning = false;   //currently all terrain objects spin, other objects don't
+    var objectIsSpinning = true;   //currently all objects spin with world
     var closestFractionAlong = 1;
 
-    processObjs(cameraRayStartPos, cameraRayEndPos, bvhObjsForWorld[world]);
+    //processObjs(cameraRayStartPos, cameraRayEndPos, bvhObjsForWorld[world]);    //non-spinning objects
+    processObjs(cameraRayStartPosSpun, cameraRayEndPosSpun, bvhObjsForWorld[world]);    //spinning objects
     processTerrain(cameraRayStartPosSpun, cameraRayEndPosSpun);
 
     function processObjs(rayStart, rayEnd, worldBvh){

commit 70a41c596acf1bc7d419ddae21855068b43b3100

fix bug introduced in f29b0a0 (stopped some objects from drawing)

js/main.js
@@ -2477,7 +2477,7 @@ function drawWorldScene(frameTime, isCubemapView, viewSettings, wSettings) {
 		{bvh:octoFrameBvh, buffers:octoFrameSubdivBuffers}
 	].forEach(objTypeInfo => {
 		var objs = bvhObjsForWorld[worldA].objList.filter(objInfo=> objInfo.bvh == objTypeInfo.bvh)	//TODO prefilter
-		if (objs>0){
+		if (objs.length>0){
 			drawArrayOfModels2(objs, objTypeInfo.buffers, activeShaderProgram);
 		}
 	});