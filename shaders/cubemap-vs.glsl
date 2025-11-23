a2f39a3c8c73ddbe613b0c2b9bccf5917f153218
Firstname Lastname <filbs111@protonmail.com>
2025-11-23 23:00:34 +0000

    fix for dark rings on portal. seems was old code

--- shaders/cubemap-vs.glsl
+++ shaders/cubemap-vs.glsl
@@ -51,74 +51,6 @@
 #endif
 #else
 		vec4 worldCoord = uMMatrix * aVertexPositionNormalized;
-#endif
-#ifdef ATMOS_ONE
-		//calculcate position on surface of portal if camera is inside portal. 
-		//todo can this be calculated more efficiently along with other terms?
-		//float fudgeSizeVal = 0.953; 	//above this w component inside portal. expect this for portal scale =0.3 because root(1-0.3*0.3). 
-										//todo pass through portal size
-		float fudgeSizeVal = 0.96; 	//works better, guess because problems with approximation
-		vec4 rayStart = uCameraWorldPos;
-		if (uCameraWorldPos.w > fudgeSizeVal){	//can lose if here if separate shader for within/out portal
-			//dumb approximation, should work for small distances - just find where line from start to end intersects plane for w=fudgeSizeVal
-			vec4 midpoint = vec4( ((uCameraWorldPos.w - fudgeSizeVal)*worldCoord.xyz + (fudgeSizeVal - worldCoord.w)*uCameraWorldPos.xyz)/(uCameraWorldPos.w-worldCoord.w) , fudgeSizeVal);
-			rayStart = normalize(midpoint);	
-		}
-		
-		/*
-		fog = 0.5*(1.0 + transformedCoord.w);
-		//note 1.0 = no fog, 0 = total fog.
-		//more complex fog calculation. show that can have radial dependence. not anything like physically correct!
-		float airDensityAtObj = exp(-dot(worldCoord.xy,worldCoord.xy));
-		fog*=airDensityAtObj; 
-		*/
-		
-		//although this is used for "per pixel" version, currently calculating fog per vertex.
-		
-		/*
-		float dotProd = dot(uCameraWorldPos,worldCoord);
-		float maxAng = acos(dotProd*0.999);	//angle from camera to object
-		float maxAngOverIters = maxAng/CONST_ITERS;
-		
-		vec4 normalDirection = normalize(worldCoord - dotProd*uCameraWorldPos);	//point 90 deg around world from camera, in direction of worldCoord
-		float totalPath=0.0;
-		float currentAngle;
-		vec4 currentPos;
-		for (float aa=0.0;aa<CONST_ITERS;aa++){
-			currentAngle=maxAngOverIters*aa;
-			currentPos = uCameraWorldPos*cos(currentAngle)+normalDirection*sin(currentAngle);
-			totalPath+=exp(uAtmosContrast*dot(currentPos.xy,currentPos.xy));	//number here affects how different atmos pressure is between extremes
-		}
-		fog=exp(-totalPath*uAtmosThickness*maxAngOverIters);
-		*/
-	
-		float dotProd = dot(rayStart,worldCoord);
-		
-		vec4 normalDirection = normalize(worldCoord - dotProd*rayStart);	//point 90 deg around world from camera, in direction of worldCoord
-		
-		float partOne = dot(rayStart.xy, rayStart.xy);
-		float partTwo = dot(normalDirection.xy, normalDirection.xy);
-		float constTerm = (partOne+partTwo)/2.0;
-		float cos2Term = (partOne-partTwo)/2.0;
-		float sin2Term = dot(rayStart.xy, normalDirection.xy);
-		float shiftAngle = atan(cos2Term,sin2Term);
-		
-		float maxDoubleAng = 2.0*acos(dotProd*0.999);
-		float maxDoubleAngOverIters = maxDoubleAng/CONST_ITERS;
-		
-		//float magTerm = sqrt(cos2Term*cos2Term+sin2Term*sin2Term);
-		float magTerm = length(vec2(cos2Term, sin2Term));
-		
-		float total=0.0;
-		for (float aa=0.5;aa<CONST_ITERS;aa++){
-			float rsq = magTerm*sin(aa*maxDoubleAngOverIters+shiftAngle);
-			total+= exp(uAtmosContrast*rsq);
-		}
-		total*= maxDoubleAngOverIters*exp(uAtmosContrast*(constTerm));	
-		fog = exp(-uAtmosThickness*total/2.0);
-
-#endif
-#ifdef ATMOS_TWO
 		float dotProd = dot(uCameraWorldPos,worldCoord);
 		
 		vec4 normalDirection = normalize(worldCoord - dotProd*uCameraWorldPos);	//point 90 deg around world from camera, in direction of worldCoord
@@ -130,11 +62,24 @@
 		float sin2Term = dot(uCameraWorldPos.xy, normalDirection.xy);
 		float shiftAngle = atan(cos2Term,sin2Term);
 		
-		float maxDoubleAng = 2.0*acos(dotProd*0.999);
-		
 		//float magTerm = sqrt(cos2Term*cos2Term+sin2Term*sin2Term);
 		float magTerm = length(vec2(cos2Term, sin2Term));
 		
+		float maxDoubleAng = 2.0*acos(dotProd);
+#endif
+#ifdef ATMOS_ONE
+		float maxDoubleAngOverIters = maxDoubleAng/CONST_ITERS;
+		
+		float total=0.0;
+		for (float aa=0.5;aa<CONST_ITERS;aa++){
+			float rsq = magTerm*sin(aa*maxDoubleAngOverIters+shiftAngle);
+			total+= exp(uAtmosContrast*rsq);
+		}
+		total*= maxDoubleAngOverIters*exp(uAtmosContrast*(constTerm));	
+		fog = exp(-uAtmosThickness*total/2.0);
+
+#endif
+#ifdef ATMOS_TWO
 		float kk = magTerm * uAtmosContrast;
 		float ksq = kk*kk;
 
